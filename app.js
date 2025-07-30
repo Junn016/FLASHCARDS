0c4b96222f7e7868bd8c1e4d0a6fa1b36ce33141
Junn016 <junior.subero@escola.pr.gov.br>
2025-07-30 12:53:07 +0000

    criamos as preguntas no javascript

---
+++ app.js
@@ -0,0 +1,6 @@
+    function criacartao (categoria, pregnta, resposta){
+       const container = document.getElementById('conteiner') 
+       const cartao =document.createElement('article')
+       cartao.className='cartao'
+
+    }
---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criacartao(
+'programação',
+'que e  filosofia ?',
+'atividade intelectual que busca compreender questões fundamentais sobre a existência, o conhecimento,a moralidade e a realidade, usando o raciocínio crítico e a argumentação'
+)
+
+criacartao(
+    'programação',
+    'o que é o tecnopolos?',
+    'é uma área geográfica onde se concentram atividades de pesquisa, desenvolvimento e inovação em alta tecnologia, geralmente envolvendo universidades eempresas.'
+)
+criacartao(
+    'programação',
+'que e a filosofia da ciência?',
+'é um ramo da filosofia que investiga a natureza, os métodos e as implicações da ciência. Ela explora questões sobre como a ciência produz conhecimento, quais são seus limites e como ela se relaciona com outras formas de conhecimento e com a sociedade.'
+)
